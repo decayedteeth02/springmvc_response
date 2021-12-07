1672ac3c8f78aed173ffefc6ef148adb2c9bc0da
cc <good@good.com>
2021-12-07 11:34:04 +0800

    viewResolver简化 逻辑视图名称

---
+++ src/main/java/cn/mvc/controllers/ResponseController.java
@@ -0,0 +1,14 @@
+package cn.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ResponseController {
+
+    @RequestMapping("/response1")
+    public String response1(){
+        return "index";
+    }
+
+}
